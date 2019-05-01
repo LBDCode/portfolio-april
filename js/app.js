dca75679ca354617784d9a9d36e1746383f92a74
LBDCode <libby.duggan@gmail.com>
2019-05-01 11:11:32 -0400

    added animations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+$(document).foundation();
+
+// $(document).ready(function(){
+
+    // $('.scroll-to-about').click(function() {
+    //     $(html, body).animate({scrollTop: $('.about').offset(top)}, 1000);
+       
+    // });
+
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   })
+
+// });
