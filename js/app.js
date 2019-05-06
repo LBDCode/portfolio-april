e041a7bbecc42b7f5a4a79c52c80be5bff0d1d4b
LBDCode <libby.duggan@gmail.com>
2019-05-06 14:01:51 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
 $(document).foundation();
-
-// $(document).ready(function(){
-
-    // $('.scroll-to-about').click(function() {
-    //     $(html, body).animate({scrollTop: $('.about').offset(top)}, 1000);
-       
-    // });
-
-    // var waypoints = $('#handler-first').waypoint(function(direction) {
-    //     notify(this.element.id + ' hit 25% from top of window') 
-    //   }, {
-    //     offset: '25%'
-    //   })
-
-// });
